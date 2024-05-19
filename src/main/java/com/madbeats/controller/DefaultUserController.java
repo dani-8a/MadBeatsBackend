470bf92f1f24cf14741d898c9da06d37987b77f8
dani-8a <danielochoa1987@gmail.com>
2024-05-19 22:54:15 +0200

    Update DefaultUserController.java

--- src/main/java/com/madbeats/controller/DefaultUserController.java
+++ src/main/java/com/madbeats/controller/DefaultUserController.java
@@ -35,20 +35,49 @@ public class DefaultUserController {
     public ResponseEntity<String> registerUser(@RequestBody DefaultUser user) {
         Optional<DefaultUser> existingUser = defaultUserRepository.findByEmail(user.getEmail());
         if (existingUser.isPresent()) {
+        	System.out.println("User already registered");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already registered");
         }
-
         defaultUserRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Usuario registrado exitosamente");
+        System.out.println("User succesfully registered");
+        return ResponseEntity.status(HttpStatus.CREATED).body("User succesfully registered");
     }
+    
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody DefaultUser user) {
+        Optional<DefaultUser> existingUser = defaultUserRepository.findByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            DefaultUser foundUser = existingUser.get();
+            if (foundUser.getPassword().equals(user.getPassword())) {
+                System.out.println("User logged: " + foundUser.getEmail());
+                return ResponseEntity.ok("User logged");
+            } else {
+            	System.out.println("Wrong password");
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Wrong password");
+            }
+        } else {
+        	System.out.println("User not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+    }
+ 
     
     @PostMapping("/{userId}/add_event/{eventId}")
     public ResponseEntity<String> addEventToFavourites(@PathVariable String userId, @PathVariable String eventId) {
         Optional<DefaultUser> optionalUser = defaultUserRepository.findById(userId);
         Optional<Event> optionalEvent = eventRepository.findById(eventId);
 
-        if (optionalUser.isEmpty() || optionalEvent.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario o evento no encontrado");
+
+        if (optionalUser.isEmpty()) {
+            String userNotFoundMessage = "User not found";
+            System.out.println(userNotFoundMessage);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFoundMessage);
+        }
+
+        if (optionalEvent.isEmpty()) {
+            String eventNotFoundMessage = "Event not found";
+            System.out.println(eventNotFoundMessage);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(eventNotFoundMessage);
         }
 
         DefaultUser user = optionalUser.get();
@@ -56,15 +85,15 @@ public class DefaultUserController {
 
         // Verificar si el evento ya está en la lista de favoritos del usuario
         if (user.getFavouritesEventList().stream().anyMatch(e -> e.getIdEvent().equals(eventId))) {
-            System.out.println("El evento ya está en la lista de favoritos del usuario");
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("El evento ya está en la lista de favoritos del usuario");
+            System.out.println("Event is already in your favourite list");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Event is already in your favourite list");
         }
 
         // Agregar el evento a la lista de favoritos del usuario y guardar los cambios
         user.getFavouritesEventList().add(event);
         defaultUserRepository.save(user);
-
-        return ResponseEntity.status(HttpStatus.OK).body("Evento agregado a la lista de favoritos del usuario");
+        System.out.println("Event added to your favourite list");
+        return ResponseEntity.status(HttpStatus.OK).body("Event added to your favourite list");
     }
     
     @GetMapping("/{userId}/favourite_events")
@@ -72,6 +101,7 @@ public class DefaultUserController {
         Optional<DefaultUser> optionalUser = defaultUserRepository.findById(userId);
 
         if (optionalUser.isEmpty()) {
+        	System.out.println("User not found");
             return ResponseEntity.notFound().build();
         }
 
@@ -79,7 +109,7 @@ public class DefaultUserController {
         List<Event> favoriteEvents = user.getFavouritesEventList();
 
         // Imprimir el nombre de cada evento por consola
-        System.out.println("Eventos favoritos del usuario:");
+        System.out.println("User favourite events:");
         favoriteEvents.forEach(event -> System.out.println(event.getNameEvent()));
 
         return ResponseEntity.ok(favoriteEvents);
@@ -90,7 +120,8 @@ public class DefaultUserController {
         Optional<DefaultUser> optionalUser = defaultUserRepository.findById(userId);
 
         if (optionalUser.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
+        	System.out.println("User not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
         }
 
         DefaultUser user = optionalUser.get();
@@ -99,7 +130,7 @@ public class DefaultUserController {
         user.getFavouritesEventList().clear();
         defaultUserRepository.save(user);
 
-        return ResponseEntity.status(HttpStatus.OK).body("Todos los eventos eliminados de la lista de favoritos del usuario");
+        return ResponseEntity.status(HttpStatus.OK).body("All favourite events deleted");
     }
     
     @DeleteMapping("/{userId}/delete_event_favourites/{eventId}")
@@ -107,8 +138,16 @@ public class DefaultUserController {
         Optional<DefaultUser> optionalUser = defaultUserRepository.findById(userId);
         Optional<Event> optionalEvent = eventRepository.findById(eventId);
 
-        if (optionalUser.isEmpty() || optionalEvent.isEmpty()) {
-            return ResponseEntity.notFound().build();
+        if (optionalUser.isEmpty()) {
+            String userNotFoundMessage = "User not found";
+            System.out.println(userNotFoundMessage);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFoundMessage);
+        }
+
+        if (optionalEvent.isEmpty()) {
+            String eventNotFoundMessage = "Event not found";
+            System.out.println(eventNotFoundMessage);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(eventNotFoundMessage);
         }
 
         DefaultUser user = optionalUser.get();
@@ -118,17 +157,16 @@ public class DefaultUserController {
 
         // Verificar si el evento está en la lista de favoritos del usuario
         if (!favoriteEvents.stream().anyMatch(e -> e.getIdEvent().equals(eventId))) {
-            return ResponseEntity.badRequest().body("El evento no está en la lista de favoritos del usuario");
+        	System.out.println("This event is not in favourites");
+            return ResponseEntity.badRequest().body("This event is not in favourites");
         }
 
         // Eliminar el evento de la lista de favoritos y guardar los cambios
         favoriteEvents.removeIf(e -> e.getIdEvent().equals(eventId));
         defaultUserRepository.save(user);
-
-        return ResponseEntity.ok("El evento ha sido eliminado de la lista de favoritos del usuario");
+        System.out.println("Event deleted from favourites");
+        return ResponseEntity.ok("Event deleted from favourites");
     }
-
-
 
 }
 
